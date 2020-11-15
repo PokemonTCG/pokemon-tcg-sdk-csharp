4f59c2f4ffad99803996ce34feaf9bf4667a05de
joergl87 <joerg.lueddemann@googlemail.com>
2020-11-15 21:19:27 +0100

    Added async to queries, tests and documentation (#15)
    * Async for queries
    
    * Added async to documentation.
    
    Co-authored-by: Jörg Lüddemann <joerg.lueddemann@outlook.de>
    
    Great work and thanks for taking the time to do this!

--- PokemonTcgSdk/Card.cs
+++ PokemonTcgSdk/Card.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using PokemonTcgSdk.Helpers;
 using PokemonTcgSdk.Mappers;
 using PokemonTcgSdk.Models;
@@ -17,6 +18,17 @@ namespace PokemonTcgSdk
         public static T Get<T>(Dictionary<string, string> query = null)
         {
             return QueryBuilder.Get<T>(query);
+        }
+
+        /// <summary>
+        ///     Async: Gets cards based on the query provided or a default list of cards.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        public static async Task<T> GetAsync<T>(Dictionary<string, string> query = null)
+        {
+            return await QueryBuilder.GetAsync<T>(query);
         }
 
         /// <summary>
@@ -41,6 +53,27 @@ namespace PokemonTcgSdk
         }
 
         /// <summary>
+        ///     Async: Gets the default list of cards or filter with a query.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        public static async Task<Pokemon> GetAsync(Dictionary<string, string> query = null)
+        {
+            try
+            {
+                var pokemon = await QueryBuilder.GetPokemonCardsAsync(query);
+                return pokemon ?? new Pokemon { Errors = new List<string> { "Not Found" } };
+            }
+            catch (Exception ex)
+            {
+                return new Pokemon
+                {
+                    Errors = new List<string> { ex.Message }
+                };
+            }
+        }
+
+        /// <summary>
         ///     Find a card based on the id provided.
         /// </summary>
         /// <typeparam name="T"></typeparam>
@@ -49,6 +82,17 @@ namespace PokemonTcgSdk
         public static T Find<T>(string id)
         {
             return QueryBuilder.Find<T>(id);
+        }
+
+        /// <summary>
+        ///     Async: Find a card based on the id provided.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public static async Task<T> FindAsync<T>(string id)
+        {
+            return await QueryBuilder.FindAsync<T>(id);
         }
 
         /// <summary>
@@ -85,6 +129,65 @@ namespace PokemonTcgSdk
                     var queryString = string.Empty;
                     var stringTask =
                         QueryBuilderHelper.BuildTaskString(query, ref queryString, client, ResourceTypes.Cards);
+                    if (stringTask.IsSuccessStatusCode)
+                    {
+                        var info = HttpResponseToPagingInfo.MapFrom(stringTask.Headers);
+                        totalCount = info.TotalCount;
+                        amount = info.Count;
+
+                        var item = QueryBuilderHelper.CreateObject<Pokemon>(stringTask);
+                        query[CardQueryTypes.Page] = (int.Parse(query[CardQueryTypes.Page]) + 1).ToString();
+                        items.Add(item);
+
+                        if (!fetchAll) break;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                // Create the list returned as a single list instead of a list of lists
+                foreach (var pokemon in items) mergedList.AddRange(pokemon.Cards);
+
+                return mergedList;
+            }
+        }
+
+        /// <summary>
+        ///     Async: Get all of the cards. This call will take a while to finish.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        public static async Task<List<PokemonCard>> AllAsync(Dictionary<string, string> query = null)
+        {
+            using (var client = QueryBuilderHelper.SetupClient())
+            {
+                var items = new List<Pokemon>();
+                var mergedList = new List<PokemonCard>();
+                var fetchAll = QueryBuilderHelper.FetchAll(ref query);
+
+                if (query != null)
+                {
+                    if (!query.ContainsKey(CardQueryTypes.Page)) query.Add(CardQueryTypes.Page, "1");
+                    query.Add(CardQueryTypes.PageSize, "500");
+                }
+                else
+                {
+                    query = new Dictionary<string, string>
+                    {
+                        {CardQueryTypes.Page, "1"},
+                        {CardQueryTypes.PageSize, "500"}
+                    };
+                }
+
+                var totalCount = int.Parse(query[CardQueryTypes.PageSize]);
+                int amount;
+                for (var i = 0; i < totalCount; i += amount)
+                {
+                    var queryString = string.Empty;
+                    var stringTask = await
+                        QueryBuilderHelper.BuildTaskStringAsync(query, queryString, client, ResourceTypes.Cards);
                     if (stringTask.IsSuccessStatusCode)
                     {
                         var info = HttpResponseToPagingInfo.MapFrom(stringTask.Headers);
---
+++ PokemonTest/PokemonAsyncTest.cs
@@ -0,0 +1,198 @@
+﻿using PokemonTcgSdk;
+using PokemonTcgSdk.Models;
+using System.Collections.Generic;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace PokemonTest
+{
+    [TestFixture]
+    public class PokemonAsyncTest
+    {
+        [Test]
+        public async Task GetAllCardsAsync()
+        {
+            var cards = await Card.AllAsync();
+            var firstCard = cards[0];
+            var name = firstCard.Name;
+
+            Assert.IsTrue(cards.Count > 8000);
+            Assert.IsFalse(string.IsNullOrWhiteSpace(name));
+            Assert.IsInstanceOf<PokemonCard>(firstCard);// IsInstanceOfType(firstCard, typeof(PokemonCard));
+        }
+
+        [Test]
+        public async Task FindPokemonCardAsync()
+        {
+            var card = await Card.FindAsync<Pokemon>("base4-4");
+            var name = card.Card.Name;
+
+            Assert.IsTrue(name == "Charizard");
+            Assert.IsNotNull(card);
+        }
+
+        [Test]
+        public async Task GetPokemonCardsAsync()
+        {
+            var cards = await Card.GetAsync<Pokemon>();
+            var name = cards.Cards[0].Name;
+
+            Assert.IsNotNull(cards);
+            Assert.IsInstanceOf<Pokemon>(cards);
+            Assert.IsFalse(string.IsNullOrWhiteSpace(name));
+        }
+
+        [TestCase("name", "Charizard")]
+        [TestCase("name", "Blastoise")]
+        [TestCase("name", "Venusaur")]
+        [TestCase("set", "sm6")]
+        public async Task GetPokemonCardsByQueryStringAsync(string field, string value)
+        {
+            var query = new Dictionary<string, string>()
+            {
+                { field, value }
+            };
+            var cards = await Card.GetAsync(query);
+
+            Assert.IsNotNull(cards);
+            Assert.IsInstanceOf<Pokemon>(cards);
+            foreach (var pokemonCard in cards.Cards)
+            {
+                switch (field)
+                {
+                    case "name":
+                        Assert.IsTrue(pokemonCard.Name.Contains(value));
+                        break;
+                    case "set":
+                        Assert.IsTrue(pokemonCard.Set.Contains(value));
+                        break;
+                    default:
+                        Assert.IsTrue(!string.IsNullOrWhiteSpace(pokemonCard.Id));
+                        break;
+                }
+            }
+        }
+
+        [Test]
+        public async Task GetTrainerCardsAsync()
+        {
+            var cards = await Card.GetAsync<Trainer>();
+            var name = cards.Cards[0].Name;
+
+            Assert.IsNotNull(cards);
+            Assert.IsInstanceOf<Trainer>(cards);
+            Assert.IsFalse(string.IsNullOrWhiteSpace(name));
+        }
+
+        [Test]
+        public async Task GetEnergyCardsAsync()
+        {
+            var cards = await Card.GetAsync<Energy>();
+            var name = cards.Cards[0].Name;
+
+            Assert.IsNotNull(cards);
+            Assert.IsInstanceOf<Energy>(cards);
+            Assert.IsFalse(string.IsNullOrWhiteSpace(name));
+        }
+
+        [Test]
+        public async Task GetSubTypesAsync()
+        {
+            var subTypes = await SubTypes.AllAsync();
+
+            Assert.IsNotNull(subTypes);
+            Assert.IsTrue(subTypes.Count > 1);
+        }
+
+        [Test]
+        public async Task GetSuperTypesAsync()
+        {
+            var superTypes = await SuperTypes.AllAsync();
+
+            Assert.IsNotNull(superTypes);
+            Assert.IsTrue(superTypes.Count > 1);
+        }
+
+        [Test]
+        public async Task GetTypesAsync()
+        {
+            var types = await Types.AllAsync();
+
+            Assert.IsNotNull(types);
+            Assert.IsTrue(types.Count > 1);
+        }
+
+        [Test]
+        public async Task GetSetsAsync()
+        {
+            var sets = await Sets.AllAsync();
+
+            Assert.IsNotNull(sets);
+            Assert.IsTrue(sets.Count > 1);
+        }
+
+        [Test]
+        public async Task FindSetsAsync()
+        {
+            var query = new Dictionary<string, string>
+            {
+                {"standardLegal", true.ToString()}
+            };
+
+            var sets = await Sets.FindAsync(query);
+            var name = sets[0].Name;
+
+            Assert.IsNotNull(sets);
+            Assert.IsFalse(string.IsNullOrWhiteSpace(name));
+            Assert.IsTrue(sets.Count >= 1);
+        }
+
+        [Test]
+        public async Task FindSetAsync()
+        {
+            var query = new Dictionary<string, string>
+            {
+                {"name", "Base"}
+            };
+
+            var sets = await Sets.FindAsync(query);
+            var name = sets[0].Name;
+
+            Assert.IsNotNull(sets);
+            Assert.IsFalse(string.IsNullOrWhiteSpace(name));
+            Assert.IsTrue(sets.Count >= 1);
+        }
+
+        [Test]
+        public async Task AllCardsBySeriesAsync()
+        {
+            var query = new Dictionary<string, string>
+            {
+                {CardQueryTypes.Series, "XY"}
+            };
+
+            var cards = await Card.AllAsync(query);
+            var name = cards[0].Name;
+
+            Assert.IsNotNull(cards);
+            Assert.IsFalse(string.IsNullOrWhiteSpace(name));
+            Assert.IsTrue(cards.Count >= 1);
+        }
+
+        [Test]
+        public async Task AllCardsByStandardLegalAsync()
+        {
+            var query = new Dictionary<string, string>
+            {
+                {SetQueryTypes.StandardLegal, true.ToString()}
+            };
+
+            var cards = await Card.AllAsync(query);
+            var name = cards[0].Name;
+
+            Assert.IsNotNull(cards);
+            Assert.IsFalse(string.IsNullOrWhiteSpace(name));
+            Assert.IsTrue(cards.Count >= 1);
+        }
+    }
+}
