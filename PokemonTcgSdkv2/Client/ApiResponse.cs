76fa5f9826bad4edcb579ab4f6ed369c3c79dc79
Serraniel <mail@serraniel.dev>
2021-08-14 20:46:33 +0200

    Added API Response class

---
+++ PokemonTcgSdkv2/Client/ApiResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace PokemonTcgSdk.Client
+{
+    public class ApiResponse
+    {
+        public int Page { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int Count { get; set; }
+
+        public int TotalCount { get; set; }
+    }
+}
