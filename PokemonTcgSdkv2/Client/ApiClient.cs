6f2b22b4404dc5eb8791554846e65ab7967cab96
Serraniel <mail@serraniel.dev>
2021-08-14 19:20:41 +0200

    Added most minimalistic client class

---
+++ PokemonTcgSdkv2/Client/ApiClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace PokemonTcgSdk.Client
+{
+    public class ApiClient
+    {
+        private readonly HttpClient _client = new HttpClient();
+
+        public ApiClient(string? apiKey)
+        {
+            _client.BaseAddress = new Uri("https://api.pokemontcg.io/v2/");
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue(
+                new ProductHeaderValue("PokemonTcgApi", GetType().Assembly.GetName().Version?.ToString())));
+            _client.DefaultRequestHeaders.Add("X-Api-Key", apiKey ?? "");
+        }
+    }
+}
