e9860f70d10256024f15381bfd05fbc060b1cdd0
Serraniel <mail@serraniel.dev>
2021-08-14 21:26:52 +0200

    Added non working test project (:

---
+++ PokemonV2Tests/PokemonTestV2.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PokemonTcgSdkV2.Client;
+
+namespace PokemonV2Tests
+{
+    [TestFixture]
+    public class PokemonTestV2
+    {
+        private ApiClient Client { get; } = new ApiClient(null);
+
+        [Test]
+        public async Task GetAllCardsAsync()
+        {
+            var cards = await Client.QueryCards("");
+
+            Assert.IsTrue(cards.Any());
+        }
+    }
+}
