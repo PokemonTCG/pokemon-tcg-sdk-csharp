c50ae2356629eb1396d4de036bc79f7c4e1c429c
Serraniel <mail@serraniel.dev>
2021-08-14 20:56:18 +0200

    First experimental request method to query cards

--- PokemonTcgSdkv2/Client/ApiClient.cs
+++ PokemonTcgSdkv2/Client/ApiClient.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+using PokemonTcgSdk.Api.Cards;
 
 namespace PokemonTcgSdk.Client
 {
@@ -15,6 +20,16 @@ namespace PokemonTcgSdk.Client
             _client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue(
                 new ProductHeaderValue("PokemonTcgApi", GetType().Assembly.GetName().Version?.ToString())));
             _client.DefaultRequestHeaders.Add("X-Api-Key", apiKey ?? "");
+        }
+
+        public async Task<IEnumerable<Card>> QueryCards(string query, int page = 1)
+        {
+            var options = new JsonSerializerOptions {PropertyNameCaseInsensitive = true, IncludeFields = true};
+
+            var cards = await _client.GetFromJsonAsync<List<Card>>($"cards?q={query}&page={page}", options) ??
+                        new List<Card>();
+
+            return cards;
         }
     }
 }
