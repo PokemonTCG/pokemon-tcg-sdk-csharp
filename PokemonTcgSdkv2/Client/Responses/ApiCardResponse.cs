963282d7827d8ab6ca2d3fac1badd2e936316b19
Serraniel <mail@serraniel.dev>
2021-08-21 19:10:34 +0200

    Small optimization of obsolete JSON Serialization Property

--- PokemonTcgSdkv2/Client/Responses/ApiCardResponse.cs
+++ PokemonTcgSdkv2/Client/Responses/ApiCardResponse.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections.Generic;
-using System.Text.Json.Serialization;
 using PokemonTcgSdkV2.Api.Cards;
 
 namespace PokemonTcgSdkV2.Client.Responses
 {
     public class ApiCardResponse : IApiResponse<Card>
     {
-        [JsonPropertyName("data")] public IEnumerable<Card> Data { get; set; }
+        public IEnumerable<Card> Data { get; set; }
 
         public int Page { get; set; }
         public int PageSize { get; set; }
