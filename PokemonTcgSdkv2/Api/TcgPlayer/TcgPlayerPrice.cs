574698b26d6ba6b2fe0fc7b3aeca2e5a04f37dc6
Serraniel <mail@serraniel.dev>
2021-08-24 22:35:40 +0200

    Added docs to the tcgplayer classes

--- PokemonTcgSdkv2/Api/TcgPlayer/TcgPlayerPrice.cs
+++ PokemonTcgSdkv2/Api/TcgPlayer/TcgPlayerPrice.cs
@@ -1,15 +1,36 @@
 ﻿namespace PokemonTcgSdkV2.Api.TcgPlayer
 {
+    /// <summary>
+    ///     The price information for card.
+    /// </summary>
+    /// <remarks>
+    ///     All prices are in US Dollars.
+    /// </remarks>
     public class TcgPlayerPrice
     {
+        /// <summary>
+        ///     The low price of the card.
+        /// </summary>
         public decimal? Low { get; set; }
 
+        /// <summary>
+        ///     The mid price of the card.
+        /// </summary>
         public decimal? Mid { get; set; }
 
+        /// <summary>
+        ///     The high price of the card.
+        /// </summary>
         public decimal? High { get; set; }
 
+        /// <summary>
+        ///     The market value of the card. This is usually the best representation of what people are willing to pay.
+        /// </summary>
         public decimal? Market { get; set; }
 
+        /// <summary>
+        ///     The direct low price of the card.
+        /// </summary>
         public decimal? DirectLow { get; set; }
     }
 }
