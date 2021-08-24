cd747703e96e925b46dac6340d92580398b2f4f1
Serraniel <mail@serraniel.dev>
2021-08-24 22:05:46 +0200

    Added docs for cardmarket classes

--- PokemonTcgSdkv2/Api/Cardmarket/CardmarketEntry.cs
+++ PokemonTcgSdkv2/Api/Cardmarket/CardmarketEntry.cs
@@ -1,11 +1,24 @@
 ﻿namespace PokemonTcgSdkV2.Api.Cardmarket
 {
+    /// <summary>
+    ///     The https://www.cardmarket.com/ information for a given card.
+    /// </summary>
     public class CardmarketEntry
     {
+        /// <summary>
+        ///     The URL to the cardmarket store page to purchase this card.
+        /// </summary>
         public string Url { get; set; }
 
+        /// <summary>
+        ///     A date that the price was last updated. In the format of YYYY/MM/DD
+        /// </summary>
         public string UpdatedAt { get; set; }
 
+        /// <summary>
+        ///     Price information for the card.
+        /// </summary>
+        /// <remarks>All prices are in Euros.</remarks>
         public CardmarketPrice Prices { get; set; }
     }
 }
