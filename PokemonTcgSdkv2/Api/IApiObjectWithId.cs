56c75e92429a59d7b811c58aed58a4e44a4f6c3e
Serraniel <mail@serraniel.dev>
2021-08-24 22:36:38 +0200

    Added docs for interface for api objects with id

--- PokemonTcgSdkv2/Api/IApiObjectWithId.cs
+++ PokemonTcgSdkv2/Api/IApiObjectWithId.cs
@@ -1,7 +1,13 @@
 ﻿namespace PokemonTcgSdkV2.Api
 {
+    /// <summary>
+    ///     Represents an object from the API which supports an id.
+    /// </summary>
     public interface IApiObjectWithId
     {
+        /// <summary>
+        ///     String representation of the objects id.
+        /// </summary>
         string Id { get; set; }
     }
 }
