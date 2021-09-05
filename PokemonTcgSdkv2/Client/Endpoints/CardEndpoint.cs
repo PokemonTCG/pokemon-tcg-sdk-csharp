a476ec3b2229455791d7bba952beaaa052fd701d
Serraniel <mail@serraniel.dev>
2021-09-05 14:54:35 +0200

    Card endpoint docs

--- PokemonTcgSdkv2/Client/Endpoints/CardEndpoint.cs
+++ PokemonTcgSdkv2/Client/Endpoints/CardEndpoint.cs
@@ -1,5 +1,10 @@
-﻿namespace PokemonTcgSdkV2.Client.Endpoints
+﻿using PokemonTcgSdkV2.Api.Cards;
+
+namespace PokemonTcgSdkV2.Client.Endpoints
 {
+    /// <summary>
+    ///     Endpoint to fetch one or more <see cref="Card" />.
+    /// </summary>
     public class CardEndpoint : ISupportsIdApiEndpoint
     {
         public string ApiUri()
