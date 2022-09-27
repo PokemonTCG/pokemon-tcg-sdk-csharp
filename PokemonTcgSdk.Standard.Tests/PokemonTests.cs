11aed6f4699d6749c3c2dc4899a957025a73b3b3
Jacob Spencer <jacob.spencer80@googlemail.com>
2022-09-27 14:39:04 +0100

    Added in unit tests

---
+++ PokemonTcgSdk.Standard.Tests/PokemonTests.cs
@@ -0,0 +1,32 @@
+﻿namespace PokemonTcgSdk.Standard.Tests
+{
+    using Moq;
+    using NUnit.Framework;
+    using PokemonTcgSdk.Standard.Infrastructure.HttpClients;
+    using PokemonTcgSdk.Standard.Infrastructure.HttpClients.Set;
+
+    public class PokemonTests
+    {
+        private PokemonApiClient _client;
+
+        [SetUp]
+        public void Setup()
+        {
+            var mockFactory = new Mock<IHttpClientFactory>();
+            var client = new HttpClient();
+            mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(client);
+            var factory = mockFactory.Object;
+            _client = new PokemonApiClient(factory.CreateClient(), string.Empty);
+        }
+
+        [Test]
+        public async Task FetchSets()
+        {
+            var response = await _client.GetSets();
+
+            Assert.IsNotNull(response);
+            Assert.IsTrue(response.Success);
+            Assert.IsTrue(response.Result.TotalCount > 0);
+        }
+    }
+}
