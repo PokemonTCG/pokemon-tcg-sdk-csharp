8508d499821c058ae56f826d58ca602fa2fb8ec3
Jacob Spencer <jacob.spencer80@googlemail.com>
2022-10-02 23:00:24 +0100

    Add built in trainer filters

---
+++ PokemonTcgSdk.Standard/Features/FilterBuilder/Trainer/TrainerFilter.cs
@@ -0,0 +1,30 @@
+﻿namespace PokemonTcgSdk.Standard.Features.FilterBuilder.Trainer;
+
+using Infrastructure.HttpClients.Cards;
+
+public static class TrainerFilter
+{
+    /// <summary>
+    /// Extension method. Will add new name filter. If name filter exists
+    /// will concat and create an OR filter. e.g "Double Rainbow Energy" or "Heal Energy"
+    /// </summary>
+    /// <param name="dictionary"></param>
+    /// <param name="value">The name value to add</param>
+    public static TrainerFilterCollection<string, string> AddName(this TrainerFilterCollection<string, string> dictionary, string value)
+    {
+        return AddOrUpdate(dictionary, nameof(EnergyCard.Name), value);
+    }
+
+    private static TrainerFilterCollection<string, string> AddOrUpdate(TrainerFilterCollection<string, string> dictionary, string key, string value)
+    {
+        if (dictionary.TryGetValue(key, out var oldValue))
+        {
+            oldValue = $"{oldValue} or {value}";
+            dictionary[key] = oldValue;
+            return dictionary;
+        }
+
+        dictionary.Add(key, value);
+        return dictionary;
+    }
+}
---
+++ PokemonTcgSdk.Standard/Features/FilterBuilder/Trainer/TrainerFilterBuilder.cs
@@ -0,0 +1,9 @@
+﻿namespace PokemonTcgSdk.Standard.Features.FilterBuilder.Trainer;
+
+public static class TrainerFilterBuilder
+{
+    public static TrainerFilterCollection<string, string> CreateSetFilter()
+    {
+        return new TrainerFilterCollection<string, string>();
+    }
+}
