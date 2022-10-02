4791c530aa53f5c5eb4b8d163893aa7b9bdbfdfa
Jacob Spencer <jacob.spencer80@googlemail.com>
2022-10-02 23:03:35 +0100

    Added missing trainer filters

--- PokemonTcgSdk.Standard/Features/FilterBuilder/Trainer/TrainerFilter.cs
+++ PokemonTcgSdk.Standard/Features/FilterBuilder/Trainer/TrainerFilter.cs
@@ -2,6 +2,7 @@
 
 using Infrastructure.HttpClients.Cards;
 
+// TODO: Legalities
 public static class TrainerFilter
 {
     /// <summary>
@@ -12,7 +13,29 @@ public static class TrainerFilter
     /// <param name="value">The name value to add</param>
     public static TrainerFilterCollection<string, string> AddName(this TrainerFilterCollection<string, string> dictionary, string value)
     {
-        return AddOrUpdate(dictionary, nameof(EnergyCard.Name), value);
+        return AddOrUpdate(dictionary, nameof(TrainerCard.Name), value);
+    }
+
+    /// <summary>
+    /// Extension method. Will add new name filter. If name filter exists
+    /// will concat and create an OR filter. e.g "Base" or "Plasma Blast"
+    /// </summary>
+    /// <param name="dictionary"></param>
+    /// <param name="value">The name value to add</param>
+    public static TrainerFilterCollection<string, string> AddSetName(this TrainerFilterCollection<string, string> dictionary, string value)
+    {
+        return AddOrUpdate(dictionary, $"{nameof(TrainerCard.Set)}:{nameof(TrainerCard.Set.Name)}", value);
+    }
+
+    /// <summary>
+    /// Extension method. Will add new name filter. If name filter exists
+    /// will concat and create an OR filter. e.g "Black & White" or "Base"
+    /// </summary>
+    /// <param name="dictionary"></param>
+    /// <param name="value">The name value to add</param>
+    public static TrainerFilterCollection<string, string> AddSetSeries(this TrainerFilterCollection<string, string> dictionary, string value)
+    {
+        return AddOrUpdate(dictionary, $"{nameof(TrainerCard.Set)}:{nameof(TrainerCard.Set.Series)}", value);
     }
 
     private static TrainerFilterCollection<string, string> AddOrUpdate(TrainerFilterCollection<string, string> dictionary, string key, string value)
