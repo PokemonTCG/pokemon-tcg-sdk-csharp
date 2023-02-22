9fa012fef58159a95ecb6d7b94e98a93df1b8cf2
Jacob Spencer <jacob.spencer80@googlemail.com>
2023-02-22 11:04:55 +0000

    Added in ancient traits support.

---
+++ PokemonTcgSdk.Standard/Infrastructure/HttpClients/Cards/Models/AncientTrait.cs
@@ -0,0 +1,12 @@
+﻿namespace PokemonTcgSdk.Standard.Infrastructure.HttpClients.Cards.Models;
+
+using Newtonsoft.Json;
+
+public class AncientTrait
+{
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("text")]
+    public string Text { get; set; }
+}
